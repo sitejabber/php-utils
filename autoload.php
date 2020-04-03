335a8c797c344e516db962e84f485e15c986393a
Ignacio Colautti <nachitox2000@hotmail.com>
2020-04-03 16:45:59 -0300

    Formatting issues fixed

--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 spl_autoload_register(function ($class) {
-    // Make sure that the class being loaded is in the sitejabber namespace
-    if (substr(strtolower($class), 0, 11) !== 'sitejabber\\')
-        return;
+	// Make sure that the class being loaded is in the sitejabber namespace
+	if (substr(strtolower($class), 0, 11) !== 'sitejabber\\')
+		return;
 
-    // Locate and load the file that contains the class
-    $path = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($path))
-        require($path);
+	// Locate and load the file that contains the class
+	$path = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
+	if (file_exists($path))
+		require($path);
 });
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,15 +3,15 @@ use PHPUnit\Framework\TestCase;
 
 class UtilsTest extends TestCase
 {
-    public function testEncryptDecripts()
-    {
+	public function testEncryptDecripts()
+	{
 		$key = 'XlVzl5iwuIakc8FMlf2Ws2XCkMs6WVHh';
 		$string = 'test string';
 		$sitejabber = new Sitejabber\Utils;
 
 		$this->assertEquals(
-            $string,
-            $sitejabber->decrypt($sitejabber->encrypt($string, $key), $key)
-        );
-    }
+			$string,
+			$sitejabber->decrypt($sitejabber->encrypt($string, $key), $key)
+		);
+	}
 }
